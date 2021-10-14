67759706168c251abb001776a542322491d716a3
Emma Chory <emmachory@gmail.com>
2021-10-14 08:49:27 -0600

    Work on book tests

--- src/book.js
+++ src/book.js
@@ -1,12 +1,27 @@
-function createTitle() {
-  
+function createTitle(title) {
+  return `The ${title}`
 }
+
+function buildMainCharacter(name, age, pronouns) {
+return {name, age, pronouns};
+}
+
+function saveReview(review, arr) {
+if(!arr.includes(review)){
+  arr.push(review);
+}
+}
+
+function calculatePageCount() {
+
+}
+  
 
 module.exports = {
   createTitle,
-  // buildMainCharacter,
-  // saveReview,
-  // calculatePageCount,
+  buildMainCharacter,
+  saveReview,
+  calculatePageCount,
   // writeBook,
   // editBook
 }
