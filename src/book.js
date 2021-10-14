4bfc63ff17f108fe97fc7732512394addf2c8042
Emma Chory <emmachory@gmail.com>
2021-10-14 09:59:32 -0600

    Complete book tests

--- src/book.js
+++ src/book.js
@@ -7,21 +7,33 @@ return {name, age, pronouns};
 }
 
 function saveReview(review, arr) {
-if(!arr.includes(review)){
+  if(!arr.includes(review)){
   arr.push(review);
 }
 }
 
-function calculatePageCount() {
+function calculatePageCount(bookTitle) {
+  var counter = 0;
+  for(let i = 0; i < bookTitle.length; i++) {
+  counter = counter + 20;
+  }
+  return counter;
+}
 
+function writeBook(title, mainCharacter, genre) {
+  return {title, mainCharacter, pageCount: calculatePageCount(title), genre}
 }
   
+function editBook(book) {
+  var quarter = book.pageCount / 4
+  book.pageCount = book.pageCount - quarter;
+}
 
 module.exports = {
   createTitle,
   buildMainCharacter,
   saveReview,
   calculatePageCount,
-  // writeBook,
-  // editBook
+  writeBook,
+  editBook
 }
