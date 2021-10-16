65723286f33ec9d705588ccee0163fc590034b8a
Emma Chory <emmachory@gmail.com>
2021-10-16 08:04:01 -0600

    Complete shelf tests

--- src/shelf.js
+++ src/shelf.js
@@ -8,11 +8,37 @@ function shelfBook(book, shelf) {
   }
 }
 
+function unshelfBook(book, shelf) {
+  for(let i = 0; i < shelf.length; i++){
+    if(book === shelf[i].title){
+      shelf.splice(i, 1)
+  }
+}
+}
+
+function listTitles(titles) {
+  var booksList = []
+{
+  for(let i = 0; i < titles.length; i++){
+    booksList.push(titles[i].title)
+  }
+  return booksList.join(", ");
+}
+}
+
+function searchShelf(shelf, book) {
+  for(let i = 0; i < shelf.length; i++) {
+    if(shelf[i].title === book){
+      return true;
+    }
+  }
+  return false;
+}
 
 
 module.exports = {
   shelfBook,
-  // unshelfBook,
-  // listTitles,
-  // searchShelf
+  unshelfBook,
+  listTitles,
+  searchShelf 
 };
