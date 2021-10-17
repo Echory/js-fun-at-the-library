fd8269c5ec161c19041dc2482b28df58a93c5ada
Emma Chory <emmachory@gmail.com>
2021-10-17 07:50:23 -0600

    Complete library tests

--- src/Library.js
+++ src/Library.js
@@ -12,13 +12,25 @@ function createLibrary(name) {
 }
 
 function addBook(library, book) {
-  if(book.genre === 'fantasy'){
-    library.shelves.fantasy.push(book)
+  library.shelves[book.genre].push(book)
+}
+
+function checkoutBook(library, title, genre) {
+  
+  for(let i = 0; i < library.shelves[genre].length; i++) {
+
+    if(title === library.shelves[genre][i].title){
+     library.shelves[genre].splice(i, 1) 
+     return `You have now checked out ${title} from the ${library.name}`
+    }
+
   }
+  return `Sorry, there are currently no copies of ${title} available at the ${library.name}`
+
 }
 
 module.exports = {
   createLibrary,
   addBook,
-  // checkoutBook
-};
+  checkoutBook
+};  
